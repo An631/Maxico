8bb140fbfb856ea4ff2aa24096cc2ce9c43dcbff
Angel <angeldm03@hotmail.com>
2018-01-15 14:15:53 -0800

    Added Images and styling to home component

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -8,31 +8,32 @@ export class DataService {
       {
       "id":1,
       "Estado": "Aguascalientes",
+      "Foto": "../../assets/Aguascalientes/main.jpg",
       "Ciudades": [
           {
             "id":1,
-            "Nombre": "San José de Gracia"
+            "Nombre": "San José de Gracia",
+            "Fotos":[
+              "../../assets/Aguascalientes/SanJosédeGracia/main.jpg",
+              "../../assests/Aguascalientes/SanJoséDeGracia/cristo_roto.jpg",
+            ],
           },
           {
             "id": 2,
-            "Nombre": "Calvillo"
+            "Nombre": "Calvillo",
+            "Fotos": [
+            "../../assets/Aguascalientes/Calvillo/main.jpg",
+            ],
           },
           {
             "id": 3,
-            "Nombre": "Real de Asientos"
+            "Nombre": "Real de Asientos",
+            "Fotos": [
+              "../../assets/Aguascalientes/RealdeAsientos/main.jpg",
+            ],
           }
         ]
-      },
-      {
-      "id":2,
-      "Estado": "Baja California",
-      "Ciudades": [
-          {
-            "id": 1,
-            "Nombre": "Tecate"
-          }
-        ]
-      },
+      }
     ]
   );
 
